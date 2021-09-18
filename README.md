cb5621dc6b725b720165fd4224595e5f04d49e29
Rioland <adedamolarioland2222@gmail.com>
2021-09-19 00:08:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gwymps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gwymps)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+import Header from "./componet/Header"
+// import HomeFace from "./componet/Face"
+
+// function Navlogo(){
+//   return 
+// }
+
+
+
+ function App() {
+  return (
+    <div>
+    <Header/>
+    </div>
+  );
+}
+export default App
